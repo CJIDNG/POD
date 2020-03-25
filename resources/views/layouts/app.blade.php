8a31efb6d37273182315d314f4bc65f8ce10e903
Faruk Nasir <frknasir@yahoo.com>
2020-03-25 16:25:55 +0100

    fix: login page crash

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ $currentTenant['platform']['display_name'] ?? $currentTenant['platform']['name'] }}</title>
+    <title>{{ app(\Hyn\Tenancy\Environment::class)->hostname()->fqdn }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -21,7 +21,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                {{ $currentTenant['platform']['display_name'] ?? $currentTenant['platform']['name'] }}
+                {{ app(\Hyn\Tenancy\Environment::class)->hostname()->fqdn }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
