946ddc2d6289e8046129a2d20da0ae10e915c10b
Faruk Nasir <frknasir@yahoo.com>
2020-06-04 01:19:53 +0100

    crafted homepage

---
+++ resources/js/components/data/TaxonomyGrid.vue
@@ -0,0 +1,45 @@
+<template>
+  <div v-if="availableItems.length > 0">
+    <div class="row row-cols-1 row-cols-md-3 row-cols-sm-2">
+      <div v-for="(item, index) in availableItems" :key="index">
+        <router-link
+          :to="'/'"
+          class="text-decoration-none"
+        >
+          <div class="col mb-4">
+            <div class="card mb-4 shadow-sm">
+              <div class="card-body">
+                <p class="card-text">{{ item.name }}</p>
+              </div>
+            </div>
+          </div>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "taxonomy-grid",
+
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      availableItems: this.items
+    };
+  },
+
+  watch: {
+    'items': function(val) {
+      this.availableItems = val
+    }
+  }
+};
+</script>
--- resources/js/statics/ptdata/Home.vue
+++ resources/js/statics/ptdata/Home.vue
@@ -2,87 +2,71 @@
   <div>
     <page-header></page-header>
 
-    <main class="py-4 text-center">
+    <main class="text-center">
+      <div class="jumbotron p-3 p-md-5 bg-primary text-center w-100">
+        <h3 class="title mb-2">
+          Welcome to PTData
+        </h3>
+        <p class="lead">
+          PTData is a data service by Premium Times Centre for Investigative 
+          Journalism. It does some amazing stuffs like curating data across many 
+          fields. You can access highly quality data. This is just an example by the 
+          way. PTData is a data service by Premium Times Centre for Investigative 
+          Journalism. It does some amazing stuffs like curating data across many 
+          fields. You can access highly quality data. This is just an example by the 
+          way.
+        </p>
+        <div class="col-md-6 px-0 mx-auto my-auto">
+          <input 
+            class="form-control" 
+            type="text" 
+            placeholder="Search" 
+            aria-label="Search"
+            v-model="query"
+          >
+        </div><br>
+      </div>
+      
       <div class="row col-xl-10 offset-xl-1 px-xl-5 col-md-12">
+        
+        <section class="section text-center w-100 mb-5">
+          <h3 class="text-center mb-5">
+            Dataset Topics
+          </h3>
+          <taxonomy-grid 
+            :items="topics"
+          />
+        </section>
 
-
-        <div class="container marketing">
-          <div class="row featurette mb-5">
-            <div class="col-md-5 order-md-2">
-              <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-              <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-            </div>
-            <div class="col-md-7 order-md-1">
-              <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-            </div>
-          </div>
-
-          <!-- Three columns of text below the carousel -->
+        <section v-if="datasets.length > 0" class="section text-center w-100 mb-5">
+          <h3 class="text-center mb-5">
+            Featured Datasets
+          </h3>
           <div class="row">
-            <div class="col-lg-4">
-              <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-              <h2>Heading</h2>
-              <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-              <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-            </div><!-- /.col-lg-4 -->
-            <div class="col-lg-4">
-              <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-              <h2>Heading</h2>
-              <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-              <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-            </div><!-- /.col-lg-4 -->
-            <div class="col-lg-4">
-              <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-              <h2>Heading</h2>
-              <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-              <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-            </div><!-- /.col-lg-4 -->
-          </div><!-- /.row -->
-
-
-          <!-- START THE FEATURETTES -->
-
-          <hr class="featurette-divider">
-
-          <div class="row featurette">
-            <div class="col-md-7">
-              <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-              <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-            </div>
-            <div class="col-md-5">
-              <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+            <div
+              v-for="(dataset, $index) in datasets"
+              :key="$index"
+              class="col-1 col-md-3"
+            >
+              <div class="mr-auto py-1">
+                <p class="mb-1">
+                  <router-link
+                    :to="{name: 'data-show', params: { id: dataset.id }}"
+                    class="font-weight-bold text-lg lead text-decoration-none"
+                  >{{ dataset.title }}</router-link>
+                </p>
+                <p class="mb-1" v-if="dataset.description">{{ trim(dataset.description, 200) }}</p>
+                <p class="text-muted mb-0">
+                  <span>{{ trans.app.author }} {{ dataset.user.name }} |</span> 
+                  <span>{{ dataset.resources.length }} {{ trans.app.resources }}</span>
+                </p>
+                <p class="text-muted mb-0">
+                  ― {{ trans.app.updated }} {{ moment(dataset.updated_at).locale(CurrentTenant.locale).fromNow() }}
+                </p>
+              </div>
             </div>
           </div>
-
-          <hr class="featurette-divider">
-
-          <div class="row featurette">
-            <div class="col-md-7 order-md-2">
-              <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-              <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-            </div>
-            <div class="col-md-5 order-md-1">
-              <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-            </div>
-          </div>
-
-          <hr class="featurette-divider">
-
-          <div class="row featurette">
-            <div class="col-md-7">
-              <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-              <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-            </div>
-            <div class="col-md-5">
-              <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-            </div>
-          </div>
-
-          <hr class="featurette-divider">
-
-          <!-- /END THE FEATURETTES -->
-
-        </div><!-- /.container -->
+        </section>
 
       </div>
     </main>
@@ -92,19 +76,28 @@
 <script>
 import moment from "moment";
 import NProgress from "nprogress";
+import TaxonomyGrid from "../../components/data/TaxonomyGrid"
 
 export default {
-  name: "contact-index",
+  name: "home-index",
 
   components: {
-    
+    TaxonomyGrid
   },
 
   data() {
     return {
       trans: JSON.parse(CurrentTenant.translations),
-      platform: CurrentTenant.platform
+      platform: CurrentTenant.platform,
+      topics: [],
+      datasets: [],
+      query: '',
     };
+  },
+
+  created() {
+    this.fetchDataTopics()
+    this.fetchFeaturedData()
   },
 
   mounted() {
@@ -112,7 +105,33 @@ export default {
   },
 
   methods: {
-    
+    fetchDataTopics() {
+      // fetch topics
+      this.request()
+        .get("/api/v1/datatopics?all=1")
+        .then(response => {
+          this.topics = response.data;
+          NProgress.done();
+        })
+        .catch(() => {
+          NProgress.done();
+        })
+    },
+
+    fetchFeaturedData() {
+      this.request()
+        .get('/api/v1/data')
+        .then(response => {
+          this.datasets = response.data.data.splice(0, 3)
+
+          NProgress.done();
+        })
+        .catch(error => {
+          // Add any error debugging...
+
+          NProgress.done();
+        });
+    },
   }
 };
 </script>
