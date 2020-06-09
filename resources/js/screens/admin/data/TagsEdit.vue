b6b9996d1873775a22b7af0d58baa6566808bfb4
Faruk Nasir <frknasir@yahoo.com>
2020-06-09 19:31:36 +0100

    added datatags components and routes

---
+++ resources/js/screens/admin/data/TagsEdit.vue
@@ -0,0 +1,199 @@
+<template>
+  <admin-page>
+    <template slot="main">
+      <page-header>
+        <template slot="status">
+          <ul class="navbar-nav mr-auto flex-row float-right">
+            <li class="text-muted font-weight-bold">
+              <span v-if="form.isSaving">{{ trans.app.saving }}</span>
+              <span v-if="form.hasSuccess" class="text-success">{{ trans.app.saved }}</span>
+            </li>
+          </ul>
+        </template>
+
+        <template slot="action">
+          <a
+            href="#"
+            class="btn btn-sm btn-outline-success font-weight-bold my-auto"
+            :class="{ disabled: form.name === '' }"
+            @click="saveTag"
+            :aria-label="trans.app.save"
+          >{{ trans.app.save }}</a>
+        </template>
+
+        <template slot="menu">
+          <div class="dropdown" v-if="id !== 'create'">
+            <a
+              id="navbarDropdown"
+              class="nav-link pr-0"
+              href="#"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="25"
+                class="icon-dots-horizontal"
+              >
+                <path
+                  class="primary"
+                  fill-rule="evenodd"
+                  d="M5 14a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm7 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm7 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
+                />
+              </svg>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+              <a
+                href="#"
+                class="dropdown-item text-danger"
+                @click="showDeleteModal"
+              >{{ trans.app.delete }}</a>
+            </div>
+          </div>
+        </template>
+      </page-header>
+
+      <main v-if="isReady" class="py-4" v-cloak>
+        <div class="col-xl-8 offset-xl-2 px-xl-5 col-md-12 mt-5">
+          <div class="form-group mb-5">
+            <div class="col-lg-12">
+              <input
+                type="text"
+                name="name"
+                autofocus
+                autocomplete="off"
+                v-model="form.name"
+                title="Name"
+                @keyup.enter="saveTag"
+                class="form-control-lg form-control border-0 px-0 bg-transparent"
+                :placeholder="trans.app.give_your_format_a_name"
+              />
+
+              <div v-if="form.errors.name" class="invalid-feedback d-block">
+                <strong>{{ form.errors.name[0] }}</strong>
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-lg-12">
+              <div v-if="form.errors.server" class="invalid-feedback d-block">
+                <strong>{{ form.errors.server[0] }}</strong>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <delete-modal
+        ref="deleteModal"
+        @delete="deleteTag"
+        :header="trans.app.delete"
+        :message="trans.app.deleted_tags_are_gone_forever"
+      ></delete-modal>
+    </template>
+  </admin-page>
+</template>
+
+<script>
+import $ from "jquery";
+import NProgress from "nprogress";
+import DeleteModal from "../../../components/global/modals/DeleteModal";
+
+export default {
+  name: "datatopics-edit",
+
+  components: {
+    DeleteModal
+  },
+
+  data() {
+    return {
+      format: null,
+      id: this.$route.params.id || "create",
+      form: {
+        id: "",
+        name: "",
+        errors: [],
+        isSaving: false,
+        hasSuccess: false
+      },
+      isReady: false,
+      trans: JSON.parse(CurrentTenant.translations)
+    };
+  },
+
+  mounted() {
+    this.fetchData();
+  },
+
+  watch: {
+    
+  },
+
+  methods: {
+    fetchData() {
+      this.request()
+        .get("/api/v1/datatags/" + this.id)
+        .then(response => {
+          this.format = response.data;
+          this.form.id = response.data.id;
+
+          if (this.id !== "create") {
+            this.form.name = response.data.name;
+          }
+
+          this.isReady = true;
+
+          NProgress.done();
+        })
+        .catch(error => {
+          this.$router.push({ name: "datatags" });
+        });
+    },
+
+    saveTag() {
+      this.form.errors = [];
+      this.form.isSaving = true;
+      this.form.hasSuccess = false;
+
+      this.request()
+        .post("/api/v1/datatags/" + this.id, this.form)
+        .then(response => {
+          this.form.isSaving = false;
+          this.form.hasSuccess = true;
+          this.id = response.data.id;
+          this.format = response.data;
+        })
+        .catch(error => {
+          this.form.isSaving = false;
+          this.form.errors = error.response.data.errors;
+        });
+
+      setTimeout(() => {
+        this.form.hasSuccess = false;
+        this.form.isSaving = false;
+      }, 3000);
+    },
+
+    deleteTag() {
+      this.request()
+        .delete("/api/v1/datatags/" + this.id)
+        .then(response => {
+          $(this.$refs.deleteModal.$el).modal("hide");
+
+          this.$router.push({ name: "datatags" });
+        })
+        .catch(error => {
+          // Add any error debugging...
+        });
+    },
+
+    showDeleteModal() {
+      $(this.$refs.deleteModal.$el).modal("show");
+    }
+  }
+};
+</script>
