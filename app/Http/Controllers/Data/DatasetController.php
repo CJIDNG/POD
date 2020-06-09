2a6a5dd139d39389b7c259be6979c066cae8904a
Faruk Nasir <frknasir@yahoo.com>
2020-06-09 19:50:51 +0100

    updated dataset controller to save tags

--- app/Http/Controllers/Data/DatasetController.php
+++ app/Http/Controllers/Data/DatasetController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Data;
 use App\Model\Data\{
   Dataset,
   Datatopic,
+  Datatag,
   Datalicense,
   Dataformat,
 };
@@ -116,6 +117,10 @@ class DatasetController extends \App\Http\Controllers\Controller
           $datasets = Datatopic::findOrFail($filterId)->datasets()->published()->latest();
           break;
 
+        case 'tags':
+          $datasets = Datatag::findOrFail($filterId)->datasets()->published()->latest();
+          break;
+
         default:
           $datasets = Dataset::latest();
           break;
@@ -152,6 +157,7 @@ class DatasetController extends \App\Http\Controllers\Controller
 
     if ($containsId || $this->isNewDataset($id)) {
       $topics = Datatopic::get(['id', 'name']);
+      $tags = Datatag::get(['id', 'name']);
       $licenses = Datalicense::get(['id', 'name']);
       $formats = Dataformat::get(['id', 'name', 'extension']);
 
@@ -171,14 +177,15 @@ class DatasetController extends \App\Http\Controllers\Controller
         ], 200);
       } else {
         $dataset = $isAdminOrDataEditor ? 
-          Dataset::with('topics')->with(['resources', 'resources.user', 'resources.format', 'license', 'user'])->find($id) :
-          Dataset::forCurrentUser()->with('topics')->with(['resources', 'resources.user', 'resources.format', 'license', 'user'])->find($id);
+          Dataset::with(['resources', 'resources.user', 'resources.format', 'license', 'user', 'topics', 'tags'])->find($id) :
+          Dataset::forCurrentUser()->with(['resources', 'resources.user', 'resources.format', 'license', 'user', 'topics', 'tags'])->find($id);
 
         event(new Viewed($dataset));
 
         return response()->json([
           'dataset' => $dataset,
           'topics' => $topics,
+          'tags' => $tags,
           'licenses' => $licenses,
           'formats' => $formats
         ], 200); 
@@ -200,16 +207,18 @@ class DatasetController extends \App\Http\Controllers\Controller
 
     if ($containsId || $this->isNewDataset($id)) {
       $topics = Datatopic::get(['id', 'name']);
+      $tags = Datatag::get(['id', 'name']);
       $licenses = Datalicense::get(['id', 'name']);
       $formats = Dataformat::get(['id', 'name', 'extension']);
 
-      $dataset = Dataset::with('topics')->with(['resources', 'resources.user', 'resources.format', 'license', 'user'])->find($id);
+      $dataset = Dataset::with()(['resources', 'resources.user', 'resources.format', 'license', 'user', 'topics', 'tags'])->find($id);
 
       event(new Viewed($dataset));
       
       return response()->json([
         'dataset' => $dataset,
         'topics' => $topics,
+        'tags' => $tags,
         'licenses' => $licenses,
         'formats' => $formats
       ], 200); 
@@ -260,6 +269,10 @@ class DatasetController extends \App\Http\Controllers\Controller
 
     $dataset->topics()->sync(
       $this->syncTopics(request('topics'))
+    );
+
+    $dataset->tags()->sync(
+      $this->syncTags(request('tags'))
     );
 
     return response()->json($dataset->refresh());
@@ -316,6 +329,35 @@ class DatasetController extends \App\Http\Controllers\Controller
         }
 
         return $topic->id;
+      })->toArray();
+    } else {
+      return [];
+    }
+  }
+
+  /**
+   * Attach or create a given tag.
+   *
+   * @param $incomingTag
+   * @return array
+   * @throws Exception
+   */
+  private function syncTags($incomingTag): array
+  {
+    if ($incomingTag) {
+      $tags = Datatag::get(['id', 'name']);
+
+      return collect($incomingTag)->map(function ($incomingTag) use ($tags) {
+        $tag = $tags->find($incomingTag['id']);
+
+        if (! $tag) {
+          $tag = Datatag::create([
+            'id' => NULL,
+            'name' => $incomingTag['name']
+          ]);
+        }
+
+        return $tag->id;
       })->toArray();
     } else {
       return [];
