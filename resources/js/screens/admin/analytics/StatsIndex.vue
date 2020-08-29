c95d47db1c5c12972d1a86ead1f282d763f1a681
Faruk Nasir <frknasir@yahoo.com>
2020-08-29 11:17:39 +0100

    updated dashboard ui

--- resources/js/screens/admin/analytics/StatsIndex.vue
+++ resources/js/screens/admin/analytics/StatsIndex.vue
@@ -1,130 +1,156 @@
 <template>
   <admin-page>
-    <template slot="main">
-      <page-header></page-header>
-
-      <main class="py-4">
-        <div class="col-xl-10 offset-xl-1 px-xl-5 col-md-12">
-          <div class="d-flex justify-content-between my-3">
-            <div class="my-3">
-              <h1>{{ trans.app.stats }}</h1>
-              <p v-if="isReady && items.length">{{ trans.app.click_to_see_insights }}</p>
-            </div>
-
-            <select
-              name
-              id
-              v-model="className"
-              @change="changeModel"
-              class="my-auto ml-auto w-auto bg-transparent custom-select border-0"
-            >
-              <option 
-                v-for="(model, index) in Object.keys(models)"
-                :key="index"
-                :value="models[model]['class']">{{ model }}</option>
-            </select>
-          </div>
-
-          <div v-if="isReady" v-cloak>
-            <div v-if="items.length">
-              <div class="card-deck mt-4">
-                <div class="card shadow bg-transparent">
-                  <div
-                    class="card-header pb-0 bg-transparent d-flex justify-content-between align-middle border-0"
-                  >
-                    <p
-                      class="font-weight-bold text-muted small text-uppercase"
-                    >{{ trans.app.views }}</p>
-                    <p>
-                      <span
-                        class="badge badge-pill badge-success p-2 font-weight-bold"
-                      >{{ trans.app.last_thirty_days }}</span>
-                    </p>
-                  </div>
-                  <div class="card-body pt-0 pb-2">
-                    <p class="card-text display-4">{{ suffixedNumber(viewCount) }}</p>
-                  </div>
+    <template slot="action">
+      <select
+        name
+        id
+        v-model="className"
+        @change="changeModel"
+        class="my-auto ml-auto w-auto custom-select border-0"
+      >
+        <option
+          v-for="(model, index) in Object.keys(models)"
+          :key="index"
+          :value="models[model]['class']"
+        >{{ model }}</option>
+      </select>
+    </template>
+    <template slot="page-title">{{ trans.app.stats }}</template>
+    <template slot="breadcrumb">
+      <breadcrumb :links="breadcrumbLinks" />
+    </template>
+    <template slot="header-content">
+      <div class="row">
+        <div class="col-xl-4 col-md-6">
+          <div class="card card-stats">
+            <!-- Card body -->
+            <div class="card-body">
+              <div class="row">
+                <div class="col">
+                  <h5 class="card-title text-uppercase text-muted mb-0">{{ trans.app.views }}</h5>
+                  <span class="h2 font-weight-bold mb-0">{{ suffixedNumber(viewCount) }}</span>
                 </div>
-                <div class="card shadow bg-transparent">
-                  <div
-                    class="card-header pb-0 bg-transparent d-flex justify-content-between align-middle border-0"
-                  >
-                    <p
-                      class="font-weight-bold text-muted small text-uppercase"
-                    >{{ trans.app.visitors }}</p>
-                    <p>
-                      <span
-                        class="badge badge-pill badge-primary p-2 font-weight-bold"
-                      >{{ trans.app.last_thirty_days }}</span>
-                    </p>
-                  </div>
-                  <div class="card-body pt-0 pb-2">
-                    <p class="card-text display-4">{{ suffixedNumber(visitCount) }}</p>
-                  </div>
-                </div>
-                <div class="card shadow bg-transparent">
-                  <div class="card-header pb-0 bg-transparent border-0">
-                    <p
-                      class="font-weight-bold text-muted small text-uppercase"
-                    >{{ trans.app.metrics }}</p>
-                  </div>
-                  <div class="card-body pt-0 pb-2">
-                    <ul>
-                      <li>{{ publishedCount }} {{ trans.app.items_found }}</li>
-                    </ul>
+                <div class="col-auto">
+                  <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
+                    <i class="ni ni-active-40"></i>
                   </div>
                 </div>
               </div>
-
-              <line-chart
-                :views="JSON.parse(viewTrend)"
-                :visits="JSON.parse(visitTrend)"
-                class="mt-5"
-              />
-
-              <div class="mt-5">
-                <div
-                  v-for="(item, $index) in items"
-                  :key="$index"
-                  class="d-flex border-top py-3 align-items-center"
-                >
-                  <div class="mr-auto">
-                    <p class="mb-1 mt-2">
-                      <router-link
-                        :to="{name: 'stats-show', params: { id: item.id, className: className }}"
-                        class="font-weight-bold text-lg lead text-decoration-none"
-                      >{{ item.title || item.name || item.description }}</router-link>
-                    </p>
-                    <p class="text-muted mb-2">
-                      <router-link
-                        :to="{name: 'stats-show', params: { id: item.id, className: className }}"
-                        class="text-decoration-none text-muted"
-                      >{{ trans.app.view_stats }}</router-link>
-                    </p>
-                  </div>
-                  <div class="ml-auto d-none d-lg-block">
-                    <span
-                      class="text-muted mr-3"
-                    >{{ suffixedNumber(item.views_count) }} {{ trans.app.views }}</span>
-                    {{ trans.app.created }} {{ moment(item.created_at).locale(CurrentTenant.locale).fromNow() }}
-                  </div>
-                </div>
-              </div>
-
-              <infinite-loading @infinite="fetchModels" spinner="spiral">
-                <span slot="no-more"></span>
-                <div slot="no-results"></div>
-              </infinite-loading>
-            </div>
-            <div v-else class="mt-5">
-              <p
-                class="lead text-center text-muted mt-5 pt-5"
-              >{{ trans.app.you_have_no_results }}</p>
-              <p class="lead text-center text-muted mt-1">{{ trans.app.stats_are_made_available }}</p>
+              <p class="mt-3 mb-0 text-sm">
+                <span class="text-success mr-2">
+                  <i class="fa fa-arrow-up"></i> --%
+                </span>
+                <span class="text-nowrap">{{ trans.app.last_thirty_days }}</span>
+              </p>
             </div>
           </div>
         </div>
-      </main>
+        <div class="col-xl-4 col-md-6">
+          <div class="card card-stats">
+            <!-- Card body -->
+            <div class="card-body">
+              <div class="row">
+                <div class="col">
+                  <h5 class="card-title text-uppercase text-muted mb-0">{{ trans.app.visitors }}</h5>
+                  <span class="h2 font-weight-bold mb-0">{{ suffixedNumber(visitCount) }}</span>
+                </div>
+                <div class="col-auto">
+                  <div class="icon icon-shape bg-gradient-orange text-white rounded-circle shadow">
+                    <i class="ni ni-single-02"></i>
+                  </div>
+                </div>
+              </div>
+              <p class="mt-3 mb-0 text-sm">
+                <span class="text-success mr-2">
+                  <i class="fa fa-arrow-up"></i> --%
+                </span>
+                <span class="text-nowrap">{{ trans.app.last_thirty_days }}</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-4 col-md-6">
+          <div class="card card-stats">
+            <!-- Card body -->
+            <div class="card-body">
+              <div class="row">
+                <div class="col">
+                  <h5 class="card-title text-uppercase text-muted mb-0">{{ trans.app.metrics }}</h5>
+                  <span class="h2 font-weight-bold mb-0">{{ publishedCount }} {{ trans.app.items_found }}</span>
+                </div>
+                <div class="col-auto">
+                  <div class="icon icon-shape bg-gradient-green text-white rounded-circle shadow">
+                    <i class="ni ni-bullet-list-67"></i>
+                  </div>
+                </div>
+              </div>
+              <p class="mt-3 mb-0 text-sm">
+                <span class="text-success mr-2">
+                  <i class="fa fa-arrow-up"></i> --%
+                </span>
+                <span class="text-nowrap">{{ trans.app.last_thirty_days }}</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+    <template slot="main">
+      <div class="col-12">
+        <div class="d-flex justify-content-between">
+          <div class="my-3">
+            <p v-if="isReady && items.length">{{ trans.app.click_to_see_insights }}</p>
+          </div>
+        </div>
+
+        <div v-if="isReady" v-cloak>
+          <div v-if="items.length">
+            <line-chart
+              :views="JSON.parse(viewTrend)"
+              :visits="JSON.parse(visitTrend)"
+              class="mt-5"
+            />
+
+            <div class="mt-5">
+              <div
+                v-for="(item, $index) in items"
+                :key="$index"
+                class="d-flex border-top py-3 align-items-center"
+              >
+                <div class="mr-auto">
+                  <p class="mb-1 mt-2">
+                    <router-link
+                      :to="{name: 'stats-show', params: { id: item.id, className: className }}"
+                      class="font-weight-bold text-lg lead text-decoration-none"
+                    >{{ item.title || item.name || item.description }}</router-link>
+                  </p>
+                  <p class="text-muted mb-2">
+                    <router-link
+                      :to="{name: 'stats-show', params: { id: item.id, className: className }}"
+                      class="text-decoration-none text-muted"
+                    >{{ trans.app.view_stats }}</router-link>
+                  </p>
+                </div>
+                <div class="ml-auto d-none d-lg-block">
+                  <span
+                    class="text-muted mr-3"
+                  >{{ suffixedNumber(item.views_count) }} {{ trans.app.views }}</span>
+                  {{ trans.app.created }} {{ moment(item.created_at).locale(CurrentTenant.locale).fromNow() }}
+                </div>
+              </div>
+            </div>
+
+            <infinite-loading @infinite="fetchModels" spinner="spiral">
+              <span slot="no-more"></span>
+              <div slot="no-results"></div>
+            </infinite-loading>
+          </div>
+          <div v-else class="mt-5">
+            <p class="lead text-center text-muted mt-5 pt-5">{{ trans.app.you_have_no_results }}</p>
+            <p class="lead text-center text-muted mt-1">{{ trans.app.stats_are_made_available }}</p>
+          </div>
+        </div>
+      </div>
     </template>
   </admin-page>
 </template>
@@ -303,15 +329,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "../../../../../resources/sass/variables";
+// @import "../../../../../resources/sass/variables";
 
-.badge-success {
-  background-color: $green-500;
-  color: darken($green, 20%);
-}
+// .badge-success {
+//   background-color: $green-500;
+//   color: darken($green, 20%);
+// }
 
-.badge-primary {
-  background-color: $blue-500;
-  color: darken($blue, 35%);
-}
+// .badge-primary {
+//   background-color: $blue-500;
+//   color: darken($blue, 35%);
+// }
 </style>
